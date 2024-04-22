90db6899ee11d628dd30424749732c52e6a2fcbe
AhmedEl-Refa3y <zeko2131340@gmail.com>
2024-04-22 12:20:14 +0200

    initial commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,11 +4,16 @@ import ScrollTop from "./ScrollTop";
 import Zoom from "react-reveal/Zoom";
 
 const Contact = () => {
-  const [message, setMessage] = useState(""); // State لتخزين الرسالة المكتوبة في الحقل textarea
-  const [subject, setSubject] = useState(""); // State لتخزين موضوع البريد الإلكتروني
+  const [message, setMessage] = useState("");
+  const [subject, setSubject] = useState("");
 
   const handleSendMessage = () => {
-    const recipient = "zekoahmed812@gmail.com"; // تغيير هذا إلى عنوان البريد الإلكتروني المستلم
+    if (!message || !subject) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    const recipient = "zekoahmed812@gmail.com";
     const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
       subject
     )}&body=${encodeURIComponent(message)}`;
@@ -57,11 +62,12 @@ const Contact = () => {
             <Zoom>
               <div className="contact__form-div">
                 <input
+                  required
                   type="text"
                   className="contact__form-input"
                   placeholder="Your Subject"
-                  value={subject} // ربط قيمة الحقل input بالمتغير subject
-                  onChange={(e) => setSubject(e.target.value)} // تحديث قيمة المتغير subject عندما يتم كتابة البيانات
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
                 />
               </div>
             </Zoom>
@@ -69,14 +75,15 @@ const Contact = () => {
             <Zoom>
               <div className="contact__form-div contact__form-area">
                 <textarea
+                  required
                   name=""
                   id=""
                   cols="30"
                   rows="10"
                   className="contact__form-input"
                   placeholder="Your Message"
-                  value={message} // ربط قيمة الحقل textarea بالمتغير message
-                  onChange={(e) => setMessage(e.target.value)} // تحديث قيمة المتغير message عندما يتم كتابة البيانات
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
               </div>
             </Zoom>
@@ -85,7 +92,11 @@ const Contact = () => {
           <div className="contact__form-group">
             <Zoom>
               <div className="contact__form-div">
-                <button className="btn" onClick={handleSendMessage}>
+                <button
+                  type="button" // تغيير النوع من submit إلى button
+                  className="btn"
+                  onClick={handleSendMessage}
+                >
                   Send Message
                 </button>
               </div>
