2184d26e5bc619ae0566f758ce0bcbd677aa96cd
AhmedEl-Refa3y <zeko2131340@gmail.com>
2024-06-08 09:10:41 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,25 @@ import BallLoader from "./components/bouncing/BallLoader";
 import Bubble from "./components/bubble/Bubble";
 
 function App() {
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll(".nav__link");
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((links) => {
+          links.classList.remove("active");
+          document
+            .querySelector(".nav__link[href*=" + id + "]")
+            .classList.add("active");
+        });
+      }
+    });
+  };
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
